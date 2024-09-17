c61580fb69982adecb8d3f9a0cc37d0cbe399b39
Prince Maurya <121998960+Krishzyx@users.noreply.github.com>
2024-09-17 18:29:03 +0530

    Create 884. Uncommon Words from Two Sentences.java

---
+++ 884. Uncommon Words from Two Sentences.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String[] uncommonFromSentences(String s1, String s2) {
+        String[] words1 = s1.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+        for(String word: words1){
+            map.put(word,map.getOrDefault(word,0)+1);
+        }
+        String[] words2 = s2.split(" ");
+        for(String word: words2){
+            map.put(word,map.getOrDefault(word,0)+1);
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        for(Map.Entry<String,Integer> mp: map.entrySet()){
+            if(mp.getValue()==1){
+                ans.add(mp.getKey());
+            }
+        }
+        return ans.toArray(new String[0]);
+    }
+}
+
+/*
+public List<String> uncommonFromSentences(String s1, String s2) {
+        // Concatenate both sentences with a space
+        String s = s1 + " " + s2;
+        
+        // Create a map to store the word counts
+        Map<String, Integer> wordCount = new HashMap<>();
+        
+        // Split the combined string into words
+        String[] words = s.split(" ");
+        
+        // Count the occurrences of each word
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+        
+        // Create a list to store the uncommon words (those with a count of 1)
+        List<String> result = new ArrayList<>();
+        
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            if (entry.getValue() == 1) {
+                result.add(entry.getKey());
+            }
+        }
+        
+        return result;
+    }
+    */
