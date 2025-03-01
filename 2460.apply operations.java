46589e9fa7bcf92a6f40dc7ae2c8a9a217a55204
Prince Maurya <121998960+Krishzyx@users.noreply.github.com>
2025-03-01 07:43:14 +0530

    Create 2460.apply operations.java

---
+++ 2460.apply operations.java
@@ -0,0 +1,29 @@
+public class Solution {
+
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Apply operations on the array
+        for (int index = 0; index < n - 1; index++) {
+            if (nums[index] == nums[index + 1] && nums[index] != 0) {
+                nums[index] *= 2;
+                nums[index + 1] = 0;
+            }
+        }
+
+        // Step 2: Shift non-zero elements to the beginning
+        int nonZeroIndex = 0;
+        for (int iterateIndex = 0; iterateIndex < n; iterateIndex++) {
+            if (nums[iterateIndex] != 0) {
+                nums[nonZeroIndex++] = nums[iterateIndex];
+            }
+        }
+
+        // Step 3: Fill the remaining positions with zeros
+        while (nonZeroIndex < n) {
+            nums[nonZeroIndex++] = 0;
+        }
+
+        return nums;
+    }
+}
