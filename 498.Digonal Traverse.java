b642c676472b3b06c47285c5b541b4a7c254816a
Prince Maurya <121998960+Krishzyx@users.noreply.github.com>
2026-03-19 22:45:29 +0530

    Implement diagonal traversal for a matrix

---
+++ 498.Digonal Traverse.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        int[] result = new int[m * n];
+        int row = 0, col = 0;
+        
+        for (int i = 0; i < m * n; i++) {
+            result[i] = mat[row][col];
+            
+            // moving up-right
+            if ((row + col) % 2 == 0) {
+                if (col == n - 1) {
+                    row++;
+                } else if (row == 0) {
+                    col++;
+                } else {
+                    row--;
+                    col++;
+                }
+            } 
+            // moving down-left
+            else {
+                if (row == m - 1) {
+                    col++;
+                } else if (col == 0) {
+                    row++;
+                } else {
+                    row++;
+                    col--;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
