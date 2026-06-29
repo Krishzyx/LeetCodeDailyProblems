68683f3a5d4ec14e296238a210611b4ed98a6c99
Prince Maurya <121998960+Krishzyx@users.noreply.github.com>
2026-06-29 11:47:13 +0530

    Time: 1196 ms (7.04%), Space: 46.9 MB (8%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -0,0 +1,30 @@
+class Solution {
+    String ans = "";
+       int count = 0;
+    public String getPermutation(int n, int k) {
+        
+        boolean[] vis = new boolean[n+1];
+        solve(n,k,"",vis);
+        return ans;
+    }
+        public void solve( int n,int k,String curr, boolean[] vis){
+            if(curr.length() == n){
+                count++;
+                if(count == k)
+                    ans = curr;
+                    return;
+                
+            }
+                if(!ans.equals(""))
+                return;
+                for(int i=1; i<=n; i++){
+                    if(!vis[i]){
+                        vis[i] = true;
+                        solve(n,k,curr+i,vis);
+                        vis[i] = false;
+                    }
+                }
+            }
+        }
+
+    
