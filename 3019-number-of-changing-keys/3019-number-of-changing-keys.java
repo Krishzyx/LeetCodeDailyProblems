00dd00451b0be0b238ae9ffffcfb8d2a657cf3f8
Prince Maurya <121998960+Krishzyx@users.noreply.github.com>
2026-07-27 20:22:29 +0530

    Time: 1 ms (100%), Space: 43.9 MB (13.35%) - LeetHub

---
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countKeyChanges(String s) {
+
+        s = s.toLowerCase();
+
+        int count = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
